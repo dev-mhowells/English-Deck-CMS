452fa3733aab4945408abce1e4108cec95779e4c
Mike <dev.mhowells@gmail.com>
2022-08-22 15:02:39 +0100

    can now save paragraph textarea value, need to ammend for multiple paragraphs. Each paragraph needs own component

--- src/Paragraphs.js
+++ src/Paragraphs.js
@@ -2,6 +2,7 @@ import "./index.css";
 import React from "react";
 
 export default function Paragraphs() {
+  const [text, setText] = React.useState("");
   const [paragraphs, setParagraphs] = React.useState([
     { number: 1, text: "", wordCount: 0 },
   ]);
@@ -13,24 +14,61 @@ export default function Paragraphs() {
     ]);
   }
 
+  // filter finds the paragraph to be deleted based on the paragraph object's
+  // number property. Map resets the number property for each paragraph object
+  // to account for the newly missing paragraph.
   function deleteParagraph(paragraphNumber) {
     setParagraphs((prevParagraphs) =>
-      prevParagraphs.filter(
-        (paragraph) => paragraph.number !== paragraphNumber + 1
-      )
+      prevParagraphs
+        .filter((paragraph) => paragraph.number !== paragraphNumber + 1)
+        .map((paragraph, index) => ({ ...paragraph, number: index + 1 }))
     );
   }
+
+  function handleText(event) {
+    setText(event.target.value);
+  }
+
+  //   function saveText(paragraphNumber) {
+  //     setParagraphs((prevParagraphs) =>
+  //       prevParagraphs.map((paragraph) => {
+  //         paragraph.number === paragraphNumber
+  //           ? { ...paragraph, text }
+  //           : paragraph;
+  //       })
+  //     );
+  //   }
+
+  function saveText(paragraphNumber) {
+    console.log("save ran with this text:", text);
+    setParagraphs((prevParagraphs) =>
+      prevParagraphs.map((paragraph) => {
+        if (paragraph.number === paragraphNumber) return { ...paragraph, text };
+        else return paragraph;
+      })
+    );
+  }
+
+  console.log("PARAGRAPHS", paragraphs);
+  console.log("TEXT", text);
 
   const allParagrpahs = paragraphs.map((paragraph, index) => (
     <div className="paragraph">
       <div className="above-para">
-        <h3>Paragraph ({index})</h3>
+        <h3>Paragraph ({paragraph.number})</h3>
       </div>
-      <textarea className="textarea"></textarea>
+      <textarea
+        className="textarea"
+        value={text}
+        onChange={handleText}
+      ></textarea>
       <div className="below-para">
         <p className="wordcount">words: 0000</p>
         <button className="delete-btn" onClick={() => deleteParagraph(index)}>
           delete
+        </button>
+        <button className="delete-btn" onClick={() => saveText(index + 1)}>
+          save
         </button>
       </div>
     </div>
