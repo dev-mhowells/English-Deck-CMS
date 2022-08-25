2885dce080d946d0f6b7e7bc573b4fd71d8d2802
Mike <dev.mhowells@gmail.com>
2022-08-25 21:52:32 +0100

    started on word component variant of vocabulary component. Thought would be better, but in fact pros and cons compared to the vocabulary component which doesn't have children

--- src/Main.js
+++ src/Main.js
@@ -2,23 +2,32 @@ import "./index.css";
 import Headings from "./Headings";
 import Paragraphs from "./Paragraphs";
 import Vocabulary from "./Vocabulary";
+import VocabularyC from "./VocabularyC";
 import Quiz from "./Quiz";
 
 import React from "react";
 
 function Main() {
-
   // State for Headings.js
-  const [title, setTitle] = React.useState("")
-  const [author, setAuthor] = React.useState("")
-  const [themes, setThemes] = React.useState("")
-  const [level, setLevel] = React.useState("")
+  const [title, setTitle] = React.useState("");
+  const [author, setAuthor] = React.useState("");
+  const [themes, setThemes] = React.useState("");
+  const [level, setLevel] = React.useState("");
 
   return (
     <main className="main">
-      <Headings title={title} setTitle={setTitle} author={author} setAuthor={setAuthor} themes={themes} setThemes={setThemes} level={level} setLevel={setLevel}/>
+      <Headings
+        title={title}
+        setTitle={setTitle}
+        author={author}
+        setAuthor={setAuthor}
+        themes={themes}
+        setThemes={setThemes}
+        level={level}
+        setLevel={setLevel}
+      />
       <Paragraphs />
-      <Vocabulary />
+      <VocabularyC />
       <Quiz />
       <button className="delete-article-btn">delete article</button>
     </main>
--- src/Word.js
+++ src/Word.js
@@ -1,10 +1,88 @@
 import React from "react";
 
 export default function Word(props) {
-  const [word, setWord] = React.useState({});
+  // const [word, setWord] = React.useState({});
+  const [word, setWord] = React.useState("");
+  const [definition, setDefinition] = React.useState("");
+  const [example, setExample] = React.useState("");
+
+  // function handleWordChange(event) {
+  //   setWord({ word: event.target.value });
+  // }
+  console.log("THIS PROPS", props.selectedObj.word);
 
   function handleWordChange(event) {
-    setWord({ word: event.target.value });
+    setWord(event.target.value);
+  }
+
+  function handleDefChange(event) {
+    setDefinition(event.target.value);
+  }
+
+  function handleExampleChange(event) {
+    setExample(event.target.value);
+  }
+
+  function clearFields() {
+    setWord("");
+    setDefinition("");
+    setExample("");
+  }
+
+  // adds a word object to the list of word objects (vocabulary)
+  function addWord() {
+    // check if word already exists before adding it
+    for (let wordObj of props.vocabulary) {
+      if (wordObj.word === word) {
+        return;
+      }
+    }
+
+    props.setVocabulary((prevVocabulary) => [
+      ...prevVocabulary,
+      {
+        word,
+        paragraphNumber: "",
+        definition,
+        example,
+        // variations: [],
+        type: "",
+      },
+    ]);
+    clearFields();
+  }
+
+  // needed to clear inputs, which clears states, then set state using passed in value
+  React.useEffect(() => {
+    clearFields();
+    setWord(props.selectedObj.word);
+  }, [props.selectedTab]);
+
+  function deleteWord(word) {
+    props.setVocabulary((prevVocabulary) =>
+      prevVocabulary.filter((vocabObj) => vocabObj.word !== props.selectedTab)
+    );
+  }
+
+  // ABOVE FINE
+  console.log("SELECTED IS", word);
+  // ----------------------------------------------------------
+
+  const [editing, setEditing] = React.useState(false);
+
+  // compares two objects, current object with fields in state defined in
+  // alreadyAdded() and the object in the vocabulary array
+  function compareObjects(obj1, obj2) {
+    // provided the keys in both objects are the same,
+    // below checks equality of values
+    for (let key of Object.keys(obj1)) {
+      console.log("KEY CHECK", obj1[key], obj2[key]);
+      if (obj1[key] && obj2[key] && obj1[key] !== obj2[key]) {
+        console.log("MISMATCH PAIR", obj1[key], obj2[key]);
+        setEditing(true);
+        return;
+      } else setEditing(false);
+    }
   }
 
   // // adds a word object to the list of word objects (vocabulary)
@@ -22,10 +100,11 @@ export default function Word(props) {
   //     ]);
   //   }
 
-  console.log("component rendered is now for:", word.word);
+  // console.log("component rendered is now for:", word.word);
+  console.log("component rendered is now for:", word);
 
   return (
-    <div>
+    <section className="word-block">
       <div className="vocab-info">
         <div className="label-input">
           <label for="word">Word</label>
@@ -34,7 +113,7 @@ export default function Word(props) {
             id={"word"}
             name={"word"}
             onChange={handleWordChange}
-            value={word.word}
+            value={word}
           ></input>
         </div>
         <div className="label-input">
@@ -46,12 +125,24 @@ export default function Word(props) {
           ></input>
         </div>
         <div className="label-input">
-          <label for={"definitions"}>Definitions</label>
-          <input type={"input"} id={"definitions"} name={"definitions"}></input>
+          <label for={"definition"}>Definition</label>
+          <input
+            type={"input"}
+            id={"definition"}
+            name={"definition"}
+            onChange={handleDefChange}
+            value={definition}
+          ></input>
         </div>
         <div className="label-input">
           <label for={"examples"}>Examples</label>
-          <input type={"input"} id={"examples"} name={"examples"}></input>
+          <input
+            type={"input"}
+            id={"examples"}
+            name={"examples"}
+            onChange={handleExampleChange}
+            value={example}
+          ></input>
         </div>
         <div className="label-input">
           <label for={"variations"}>Variations</label>
@@ -63,11 +154,14 @@ export default function Word(props) {
         </div>
       </div>
       <div className="below-vocab">
-        <button className="delete-word">delete word</button>
+        {editing && <button className="save-word">save edit</button>}
+        <button className="delete-word" onClick={deleteWord}>
+          delete word
+        </button>
       </div>
-      <button className="new-btn" onClick={() => props.addWord2(word)}>
+      <button className="new-btn" onClick={addWord}>
         +
       </button>
-    </div>
+    </section>
   );
 }
