7d733ad819ac25024abf6fa0c1f6ec7ee2d5eb05
Mike <dev.mhowells@gmail.com>
2022-08-25 19:54:08 +0100

    implemented check for whether content already created and saved as objects in vocabulary arr state are being changed, if so, save changes button appears. Breaks when checking whether arrays in objects match

--- src/Vocabulary.js
+++ src/Vocabulary.js
@@ -43,7 +43,7 @@ export default function Vocabulary() {
         paragraphNumber: "",
         definition,
         example,
-        variations: [],
+        // variations: [],
         type: "",
       },
     ]);
@@ -90,18 +90,52 @@ export default function Vocabulary() {
 
   const [editing, setEditing] = React.useState(false);
 
+  const obj1 = {
+    one: 1,
+    two: 2,
+    three: "",
+  };
+  const obj2 = {
+    one: 1,
+    two: 2,
+    three: "",
+  };
+
+  // compares two objects, current object with fields in state defined in
+  // alreadyAdded() and the object in the vocabulary array
+  function compareObjects(obj1, obj2) {
+    // provided the keys in both objects are the same,
+    // below checks equality of values
+    for (let key of Object.keys(obj1)) {
+      console.log("KEY CHECK", obj1[key], obj2[key]);
+      if (obj1[key] && obj2[key] && obj1[key] !== obj2[key]) {
+        console.log("MISMATCH PAIR", obj1[key], obj2[key]);
+        setEditing(true);
+        return;
+      } else setEditing(false);
+    }
+  }
+
+  // checks if the currently seleced tab/wordObj is already saved
   function alreadyAdded() {
+    // array commented out because resluts in the comparison coming out false,
+    // even if both arrays are empty. probably something to do with shallow and
+    // deep comparison
+
+    // represents current display inside vocab ie all state which populates fields
+    // are turned into this object
     let currentState = {
       paragraphNumber: "",
-      variations: [],
+      //   variations: [],
       type: "",
       word,
       definition,
       example,
     };
 
+    // saved obj becomes the currently selected object from vocab array
     let savedObj;
-
+    // sets savedObj based on currently selected tab to find matchin obj in vocab arr
     for (let wordObj of vocabulary) {
       if (wordObj.word === selectedTab) {
         savedObj = wordObj;
@@ -110,11 +144,8 @@ export default function Vocabulary() {
 
     console.log("current state", currentState, "savedObj", savedObj);
 
-    currentState.word !== savedObj.word ? setEditing(true) : setEditing(false);
-    console.log(
-      "DOES CURRENT STATE = SAVED OBJ?",
-      currentState.word === savedObj.word
-    );
+    // need if statement because on first load, savedObj is undefined(empty)
+    if (savedObj) compareObjects(currentState, savedObj);
   }
 
   console.log("editing", editing);
@@ -186,6 +217,7 @@ export default function Vocabulary() {
         </div>
       </div>
       <div className="below-vocab">
+        {editing && <button className="save-word">save edit</button>}
         <button className="delete-word" onClick={deleteWord}>
           delete word
         </button>
